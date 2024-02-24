0b8412d2ca3ce2f86606dfabd1aca548d3611405
Приходько Владислав <3885vlad@gmail.com>
2024-02-24 13:22:55 +0200

    Deleted unused imports

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -3,12 +3,8 @@ package ua.edu.lntu.cw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
