40c3559ef4c8680326428ab32172b73909d7528b
Приходько Владислав <3885vlad@gmail.com>
2024-02-24 13:13:04 +0200

    Upgraded

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -3,17 +3,29 @@ package ua.edu.lntu.cw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +38,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Body() {
-    val items = listOf("1", "sdg", "sdfsd", "sd");
+    val items = listOf(
+        "Bread",
+        "Cheese",
+        "Butter",
+        "Phone",
+        "Item 1",
+        "Item 2",
+        "Item 3",
+        "Item 4",
+        "Item 5",
+        "Item 6",
+        "Item 7",
+        "Item 8",
+        "Item 9",
+        "Item 10",
+        "Item 11",
+        "Item 12"
+    );
 
     LazyColumn(
         modifier = Modifier
@@ -42,10 +71,32 @@ fun Body() {
 fun ColumnListItem(rowIndex: Int, text: String) {
     Row(
         modifier = Modifier
-            .padding(4.dp)
+            .padding(4.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
+        Button(
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp),
+            onClick = {}
+        ) { }
+
         Text(
-            text = text
+            modifier = Modifier
+                .padding(4.dp),
+            text = text,
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 40.sp,
+                letterSpacing = 0.1.sp,
+                background = Color.DarkGray,
+                fontFamily = FontFamily.Cursive
+            )
+        )
+
+        Text(
+            text = "Text abcd",
+            style = MaterialTheme.typography.titleSmall
         )
     }
 }
